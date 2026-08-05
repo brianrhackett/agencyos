8aabd9e2f9d7b9ab4299730d952160327ad68a4c
Brian Hackett <brianrhackett83@gmail.com>
2026-08-05 16:44:29 -0400

    LOGOS and tailwind and created landing page template

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::view('/', 'landing');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
